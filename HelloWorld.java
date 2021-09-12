0f7aeca02f7b4dcaa7912a449e098a79a27838a3
kanchanap <kanchanap@gmail.com>
2021-09-12 23:54:09 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-public class Simple{
+public class HelloWorld{
 public static void main(String args[]){
-System.out.println("hello javatpoint");
+System.out.println("hello Kanchana here .........");
 }}
